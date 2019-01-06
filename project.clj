5422b7b9b469efbc2d01f06bfc64b1513a887356
Wanishing <tal.vanish@gmail.com>
2019-01-06 20:33:17 +0200

    temp

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ticktok "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "ticktok clojure client"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -20,4 +20,28 @@
                  []))
   :main ^:skip-aot ticktok.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :deploy-repositories   :deploy-repositories [["releases"
+                                                {:url
+                                                 "https://api.bintray.com/content/ticktok-io/maven/ticktok-clojure-client/"
+                                                 :sign-releases false
+                                                 :username :env/bintray_username
+                                                 :password :env/bintray_api_key}]
+                                               ["snapshots"
+                                                {:url "https://api.bintray.com/content/ticktok-io/maven/ticktok-clojure-client/"
+                                                 :sign-releases false
+                                                 :username :env/bintray_username
+                                                 :password :env/bintray_api_key}]]
+
+
+  [["releases"
+                         {:url
+                         "https://dl.bintray.com/ticktok-io/maven/ticktok-clojure-client/;publish=1"
+                          :sign-releases false
+                          :username :env/bintray_username
+                          :password :env/bintray_api_key}]
+                        ["snapshots"
+                         {:url "https://dl.bintray.com/ticktok-io/maven/ticktok-clojure-client/;publish=1"
+                          :sign-releases false
+                          :username :env/bintray_username
+                          :password :env/bintray_api_key}]])
