1e0b11fb52a08b5df76f707038883d832258dc48
Wanishing <tal.vanish@gmail.com>
2019-01-18 13:57:27 +0200

    exp

--- src/ticktok/core.clj
+++ src/ticktok/core.clj
@@ -1,9 +1,21 @@
 (ns ticktok.core
   (require [org.httpkit.client :as http]
            [clojure.data.json :as json]
-           [clojure.string :as string]))
+           [clojure.string :as string]
+           [clojure.spec.alpha :as s]))
 
 (def api "/api/v1/clocks")
+
+(def schedule-regex #"[a-zA-Z]+.[1-9][0-9]*+.[a-zA-Z]+")
+
+(s/def ::schedule-type (s/and string? #(re-matches schedule-regex %)))
+
+(s/def ::name string?)
+
+(s/def ::schedule ::schedule-type)
+
+(s/def clock-type
+  (s/keys :req [::name ::schedule]))
 
 (defn ticktok [host clock]
   (let [options {:headers  {"Content-Type" "application/json"}
