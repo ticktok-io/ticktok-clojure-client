af911e8f2e81925b0b4fe18317af1c428cbae423
Wanishing <tal.vanish@gmail.com>
2019-02-15 13:18:31 +0200

    update lein task

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,9 +6,9 @@ echo "Configing git credentials"
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
 
-if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "release-0" ]; then
+if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "release-*" ]; then
     echo going to deploy
-    lein deploy clojars
+    lein release
     echo deployed successfully
 else
     echo No publishing from $CIRCLE_BRANCH branch
