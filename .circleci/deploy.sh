778ccc38ef2f435f082c6603af95f9ed6b7a0779
Wanishing <tal.vanish@gmail.com>
2019-02-15 12:45:14 +0200

    d

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -15,7 +15,7 @@ git config --global user.name "${GIT_USERNAME}"
 
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "release-0" ]; then
     echo going to deploy
-    lein deploy
+    lein deploy clojars
     echo deployed successfully
 else
     echo No publishing from $CIRCLE_BRANCH branch
