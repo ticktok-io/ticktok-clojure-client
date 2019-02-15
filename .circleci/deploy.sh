197ab9604be4d27bf9fbe4167161bca402fe52e7
Wanishing <tal.vanish@gmail.com>
2019-02-15 12:28:05 +0200

    testing deploy from ci

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
+echo "Creating lein profiles.clj"
+echo "{:user {}
+ :auth {:repository-auth {#"clojars" {:username #=(eval (System/getenv "CLOJARS_USERNAME"))
+                                      :password  #=(eval (System/getenv "CLOJARS_PASSWORD"))}}}}" > ~/.lein/profiles.clj
+echo "Config git credentials"
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_USERNAME}"
 
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == realease-* ]; then
     echo going to deploy
-    lein release
+    lein deploy
     echo deployed successfully
 else
     echo No publishing from $CIRCLE_BRANCH branch
