84c53f79b20feb40206baef0b6dfb38fc89eb743
Wanishing <tal.vanish@gmail.com>
2019-03-04 19:23:07 +0200

    split types

--- src/ticktok/core.clj
+++ src/ticktok/core.clj
@@ -1,37 +1,9 @@
 (ns ticktok.core
   (:require [org.httpkit.client :as http]
            [clojure.data.json :as json]
-           [clojure.string :as string]
-           [clojure.spec.alpha :as s]))
+           [clojure.string :as string]))
 
 (def api "/api/v1/clocks")
-
-(def schedule-regex #"every.[1-9].\w+")
-
-(s/def ::schedule-type
-  (s/and string? #(re-matches schedule-regex %)))
-
-(s/def ::name string?)
-
-(s/def ::schedule ::schedule-type)
-
-(s/def ::clock-type (s/keys :req [::name ::schedule]))
-
-(def clock {::name "myclock"
-            ::schedule "every.5.seconds"})
-
-(def bad-clock {::name "myclock"
-                ::schedule "every.0.seconds"})
-
-(s/valid? ::clock-type
-          clock)
-
-(s/explain ::clock-type
-           bad-clock)
-
-(s/conform ::clock-type
-           clock)
-
 
 (defn ticktok [host clock]
   (let [options {:headers  {"Content-Type" "application/json"}
---
+++ src/ticktok/domain.clj
@@ -0,0 +1,25 @@
+(ns ticktok.domain
+  (:require
+            [clojure.string :as string]
+            [clojure.spec.alpha :as s]))
+
+(def schedule-regex #"every.[1-9].\w+")
+
+(s/def ::schedule-type
+  (s/and string? #(re-matches schedule-regex %)))
+
+(s/def ::name string?)
+
+(s/def ::schedule ::schedule-type)
+
+(s/def ::id string?)
+
+(s/def ::queue string?)
+
+(s/def ::uri string?)
+
+(s/def ::channel (s/keys [::queue ::uri]))
+
+(s/def ::clock-request (s/keys :req [::name ::schedule]))
+
+(s/def ::clock (s/keys :req [::channel ::id ::name ::schedule ::url]))
