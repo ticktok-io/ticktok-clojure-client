5045f95cc164732e4c8c8936b6ec7f38d1572558
Wanishing <tal.vanish@gmail.com>
2019-03-05 21:44:03 +0200

    refactoring

--- test/ticktok/core_test.clj
+++ test/ticktok/core_test.clj
@@ -18,11 +18,14 @@
 (defn stub-ticktok []
   (get @state :stub-ticktok))
 
+(defn stub-ticktok-is-not-found []
+  (stub/respond-with (stub-ticktok) {:status 404}))
+
 (facts "about ticktok"
        (with-state-changes [(before :facts (start-ticktok))
                             (after :facts (stop-ticktok))]
          (fact "should fail if ticktok server not found"
-               (prerequisite (stub/respond-with (stub-ticktok) {:status 404}) => nil)
+               (prerequisite (stub-ticktok-is-not-found)) => nil)
                (let [clock {:name "myclock"
                             :schedule "Every.5.Seconds"}]
                  (ticktok host clock) => false
