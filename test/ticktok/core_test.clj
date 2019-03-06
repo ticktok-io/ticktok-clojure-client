567d421f1ce009a09575ae508bf1c94e7567205f
Wanishing <tal.vanish@gmail.com>
2019-03-06 11:53:27 +0200

    added test

--- test/ticktok/core_test.clj
+++ test/ticktok/core_test.clj
@@ -1,8 +1,10 @@
 (ns ticktok.core-test
   (:require [clojure.test :refer :all]
             [ticktok.core :refer :all]
+            [ticktok.domain :refer :all]
             [ticktok.stub-ticktok :as stub]
-            [midje.sweet :refer :all])
+            [midje.sweet :refer :all]
+            [clojure.data.json :as json])
   )
 
 (def host  "http://localhost:8080")
@@ -21,12 +23,28 @@
 (defn stub-ticktok-is-not-found []
   (stub/respond-with (stub-ticktok) {:status 404}))
 
+(defn make-clock-request []
+  {:name "myclock"
+   :schedule "Every.5.Seconds"})
+
+(defn make-clock-from [clock-req]
+  (let [body {::channel {::queue "queue.it"
+                         ::uri "rabbit.uri"}
+              ::name (:name clock-req)
+              ::schedule (:schedule clock-req)}]
+    {:status 201
+     :body (json/write-str body)}))
+
+
 (facts "about ticktok"
        (with-state-changes [(before :facts (start-ticktok))
                             (after :facts (stop-ticktok))]
-         (fact "should fail if ticktok server not found"
-               (prerequisite (stub-ticktok-is-not-found) => nil)
-               (let [clock {:name "myclock"
-                            :schedule "Every.5.Seconds"}]
-                 (ticktok host clock) => false
-                 (:body (stub/incoming-request (stub-ticktok))) => clock))))
+         (let [clock-request (make-clock-request)]
+           (fact "should fail if ticktok server not found"
+                 (prerequisite (stub-ticktok-is-not-found) => nil)
+                 (ticktok host clock-request) => false
+                 (:body (stub/incoming-request (stub-ticktok))) => clock-request)
+           (let [clock (make-clock-from clock-request)]
+             (fact "should return clock details"
+                   (prerequisite (stub/respond-with clock) => nil)
+                   (ticktok host clock-request) => (:body clock))))))
