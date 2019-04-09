06415a7dc1aad838af3d65c8d882525c7e07c830
Wanishing <tal.vanish@gmail.com>
2019-04-09 11:41:06 +0300

    configing git credentials

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 if [ "${CIRCLE_BRANCH}" == "release" ]; then
     echo going to deploy
+    git config --global user.email "tal.vanish@gmail.com"
+    git config --global user.name "wanishing"
     lein release
     echo deployed successfully
 else
