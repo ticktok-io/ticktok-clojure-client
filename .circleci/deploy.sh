febd104993e5da761774fa8e7067c945f939094d
Wanishing <tal.vanish@gmail.com>
2019-04-09 12:04:21 +0300

    set upstream repository

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,6 +6,7 @@ if [ "${CIRCLE_BRANCH}" == "release-t" ]; then
     echo going to deploy
     git config --global user.email "tal.vanish@gmail.com"
     git config --global user.name "wanishing"
+    git push --set-upstream origin release
     lein release
     echo deployed successfully
 else
