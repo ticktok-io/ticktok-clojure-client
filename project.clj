64ca5c9421abc374aeb973444bfca242e8dff61f
Wanishing <tal.vanish@gmail.com>
2019-04-09 11:29:08 +0300

    update snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ticktok/ticktok "0.1.0-SNAPSHOT"
+(defproject ticktok/ticktok "0.1.1-SNAPSHOT"
   :description "ticktok clojure client"
   :url "https://ticktok.io"
   :license {:name "Eclipse Public License"
