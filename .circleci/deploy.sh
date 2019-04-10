ac74f5b751e1de5aba33745c984560343d18e9d4
Wanishing <tal.vanish@gmail.com>
2019-04-10 21:43:38 +0300

    remove endless release loop

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-if [ "${CIRCLE_BRANCH}" == "release" ]; then
+if [ "${CIRCLE_BRANCH}" == "release-t" ]; then
     echo going to deploy
     git config --global user.email "tal.vanish@gmail.com"
     git config --global user.name "wanishing"
-    git push --set-upstream origin release
     lein release
     echo deployed successfully
 else
