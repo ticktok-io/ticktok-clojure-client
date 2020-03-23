b40e18d36d9f7e966165942ff0b7fcf641735306
Tal Vanish <tvanish@movez.ai>
2020-03-23 19:55:42 +0200

    upgrade http kit api

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [http-kit "2.2.0"]
+                 [http-kit "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.novemberain/langohr "5.0.0"]
                  [com.grammarly/perseverance "0.1.3"]
